6c82aacce751e94aa645c2bff9cb2331564f399a
lazzzis <shenlijin@outlook.com>
2017-04-29 23:22:56 +0800

    add a basic query for problem list

---
+++ models/Problem.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate')
+
+const ProblemSchema = mongoose.Schema({
+  pid: {
+    type: Number,
+    index: {
+      unique: true // 建立索引，加快查询速度
+    }
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: Number,
+    default: 1000
+  },
+  memory: {
+    type: Number,
+    default: 32768
+  },
+  create: {
+    type: String,
+    default: Date.now
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  input: {
+    type: String,
+    default: ''
+  },
+  output: {
+    type: String,
+    default: ''
+  },
+  in: {
+    type: String,
+    default: ''
+  },
+  out: {
+    type: String,
+    default: ''
+  },
+  solve: {
+    type: Number,
+    default: 0
+  },
+  submit: {
+    type: Number,
+    default: 0
+  },
+  argument: {
+    type: String
+  }
+}, {
+  collection: 'Problem'
+})
+
+ProblemSchema.plugin(mongoosePaginate)
+
+module.exports = mongoose.model('Problem', ProblemSchema)
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const Router = require('koa-router')
 const api = new Router()
 
 const contests = require('./contests')
+const problems = require('./problems')
 
-api.use('/api', contests.routes(), contests.allowedMethods())
+api
+  .use('/api', contests.routes(), contests.allowedMethods())
+  .use('/api', problems.routes(), problems.allowedMethods())
 
 module.exports = function (app) {
   app.use(api.routes())
