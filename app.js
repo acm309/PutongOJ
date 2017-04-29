61e75e1d4d5411ab3a5ce2ebf12985ac6279c36d
lazzzis <shenlijin@outlook.com>
2017-04-29 16:38:53 +0800

    add a route template and some comments

--- app.js
+++ app.js
@@ -10,17 +10,20 @@ const redisStore = require('koa-redis')
 const path = require('path')
 
 const config = require('./config')
+const appRouter = require('./routes')
 
 const app = new Koa()
 
-// inject the config
+// 注入 config，在后续路由或中间件中，可以直接使用 ctx.config 访问
 app.use(async function (ctx, next) {
   ctx.config = config
   await next()
 })
 
+// 日志，会在控制台显示请求的方法和路由
 app.use(logger())
 
+// 上传文件的文件夹地址
 app.use(koaBody({
   formidable: {
     uploadDir: path.resolve(__dirname, 'public/uploads')
@@ -30,12 +33,12 @@ app.use(koaBody({
 }))
 
 app.use(convert(staticCache(path.resolve(__dirname, 'public'), {
-  maxAge: 7 * 24 * 60 * 60
+  maxAge: 7 * 24 * 60 * 60 // 7 天不更新，也就是缓存期限
 })))
 
 app.use(serve('public'))
 
-app.keys = ['FlandreScarlet']
+app.keys = ['FlandreScarlet'] // session 加密需要
 
 app.use(session({
   store: redisStore({
@@ -50,16 +53,19 @@ app.use(session({
   }
 }))
 
+// 错误报告，controller 提出的错务必提供 message，只有未知的错误才会显示 "Unknown..."
 app.use(async function (ctx, next) {
   try {
     await next()
   } catch (err) {
     ctx.status = err.status || 500
     ctx.body = {
-      error: err.message
+      error: err.message || 'Unknown message, please contact the admin'
     }
   }
 })
+
+appRouter(app)
 
 console.log(`listening on port ${config.port}`)
 
