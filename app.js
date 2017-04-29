4413c96de91e8f6ceca4714e37715f4b68396417
lazzzis <shenlijin@outlook.com>
2017-04-29 14:50:01 +0800

    inject config and build structure

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const koa = require('koa')
+const Koa = require('koa')
+const config = require('./config')
 
-const app = new koa()
+const app = new Koa()
+
+// inject the config
+app.use(async function (ctx, next) {
+  ctx.config = config
+  await next()
+})
 
 app.use(async function (ctx, next) {
-  ctx.body = "hello koa"
+  ctx.body = 'hello koa'
+  await next()
 })
 
 app.listen(3000)
