6527c1ceac2250bcac375452a0e34b40d2a45198
lazzzis <shenlijin@outlook.com>
2017-04-29 21:28:00 +0800

    a support for condition query for the contest list

--- controllers/contests.js
+++ controllers/contests.js
@@ -1,13 +1,27 @@
 const Contest = require('../models/Contest')
 
 async function queryList (ctx, next) {
+  const filter = {} // 用于 mongoose 的筛选
+
+  if (ctx.query.field) {
+    // https://docs.mongodb.com/manual/reference/operator/query/where/
+    // field 一般为 cid 或 title
+    // ctx.query.query 表示搜获的内容
+    // 使用 string 而不是 function 是因为，这里的 function 是不能访问到外部变量的
+    // 用模板字符串生成筛选条件
+    // 正则可以匹配更多的内容
+    filter.$where =
+      `${new RegExp(ctx.query.query, 'i')}.test(this["${ctx.query.field}"])`
+  }
+
   const res = await Contest
-    .paginate({}, {
+    .paginate(filter, {
       limit: +ctx.query.limit || 30, // 加号表示使其变为数字
       page: +ctx.query.page || 1,
       sort: {cid: -1},
       select: 'title cid start end status encrypt'
     })
+
   ctx.body = {
     contests: res.docs,
     pagination: {
