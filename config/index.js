1455afed2aa1a9dfc2d8796668682d810f4df01c
lazzzis <shenlijin@outlook.com>
2017-04-30 13:43:11 +0800

    use local db

--- config/index.js
+++ config/index.js
@@ -7,6 +7,6 @@ config.redisUrl = 'redis://localhost:6379'
 // 连接前记得配置 mongod, 如果是远端数据库
 // http://stackoverflow.com/questions/13312358/mongo-couldnt-connect-to-server-127-0-0-127017
 // 修改 /etc/mongod.conf； 注释 bind_ip 与 port
-config.mongoUrl = 'mongodb://10.131.102.14:27017/oj'
+config.mongoUrl = 'mongodb://127.0.0.1:27017/oj'
 
 module.exports = config
