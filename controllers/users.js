019f14105474ce94106816cb815d572caf97e62e
lazzzis <shenlijin@outlook.com>
2017-04-30 14:08:39 +0800

    add solved and unsolved property for query one user

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const User = require('../models/User')
+const Solution = require('../models/Solution')
+const _ = require('ramda')
 
 async function queryOneUser (ctx, next) {
   const uid = ctx.params.uid
@@ -6,14 +8,31 @@ async function queryOneUser (ctx, next) {
   const user = await User
     .findOne({uid})
     .select('-_id uid nick solve submit status timerecord iprecord school mail motto privilege')
+    .lean()
     .exec()
 
   if (!user) {
     ctx.throw(400, 'No such a user')
   }
 
+  let solved = await Solution
+    .find({uid, judge: 3}) // TODO: fix this number to a constant variable
+    .distinct('pid')
+    .exec()
+
+  solved = _.sort((x, y) => (x < y ? -1 : 1), solved)
+
+  let unsolved = await Solution
+    .find({uid, judge: {$ne: 3}}) // TODO: fix this number to a constant variable
+    .distinct('pid')
+    .exec()
+
+  unsolved = _.filter((pid) => !solved.includes(pid), unsolved)
+
   ctx.body = {
-    user
+    user,
+    solved,
+    unsolved
   }
 }
 
