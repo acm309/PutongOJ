7c016e8e7aca8e63f15d56ef1fa1b66c5f19fab7
lazzzis <shenlijin@outlook.com>
2017-05-04 17:39:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <oj-nav></oj-nav>
+  </div>
+</template>
+
+<script>
+
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'oj-nav': Nav
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <nav class="nav has-shadow">
+    <div class="container">
+      <div class="nav-left">
+        <a class="nav-item is-tab is-hidden-mobile">Home</a>
+        <a class="nav-item is-tab is-hidden-mobile">Problems</a>
+        <a class="nav-item is-tab is-hidden-mobile">Status</a>
+        <a class="nav-item is-tab is-hidden-mobile">Ranklist</a>
+        <a class="nav-item is-tab is-hidden-mobile">Contests</a>
+        <a class="nav-item is-tab is-hidden-mobile">FAQ</a>
+      </div>
+      <span class="nav-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+      <div class="nav-right nav-menu">
+        <a class="nav-item is-tab is-hidden-tablet is-active">Home</a>
+        <a class="nav-item is-tab is-hidden-tablet">Problems</a>
+        <a class="nav-item is-tab is-hidden-tablet">Status</a>
+        <a class="nav-item is-tab is-hidden-tablet">Ranklist</a>
+        <a class="nav-item is-tab is-hidden-tablet">Contests</a>
+        <a class="nav-item is-tab is-hidden-tablet">FAQ</a>
+        <a class="nav-item is-tab">Emerald</a>
+        <a class="nav-item is-tab">Log out</a>
+        <a class="nav-item is-tab">Admin</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
