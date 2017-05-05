22b3fa158aef8e3cf1f6e9a8ab5b01307256665b
lazzzis <shenlijin@outlook.com>
2017-05-05 15:58:49 +0800

    solutions sort

--- src/router.js
+++ src/router.js
@@ -40,7 +40,11 @@ const routes = [
     name: 'status',
     props: (route) => ({
       page: route.query.page,
-      limit: route.query.limit
+      limit: route.query.limit,
+      uid: route.query.uid,
+      pid: route.query.pid,
+      language: route.query.language,
+      judge: route.query.judge
     })
   }, {
     path: '/ranklist',
--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,5 +1,62 @@
 <template lang="html">
   <div>
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">User</label>
+      </div>
+      <div class="field-body">
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Username" v-model="searchUid">
+          </p>
+        </div>
+        <div class="field-label is-normal">
+          <label class="label">Pid</label>
+        </div>
+        <div class="field">
+          <p class="control is-expanded">
+            <input class="input" type="text" placeholder="Problem id" v-model="searchPid">
+          </p>
+        </div>
+        <div class="field-label is-normal">
+          <label class="label">Judge</label>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-left has-icons-right">
+            <div class="select">
+              <select v-model="searchJudge">
+                <option :value="undefined">ALL</option>
+                <option
+                  v-for="(name, code) in JUDGES"
+                  :value="code">{{ name }}</option>
+              </select>
+            </div>
+          </p>
+        </div>
+        <div class="field-label is-normal">
+          <label class="label">Language</label>
+        </div>
+        <div class="field">
+          <p class="control is-expanded has-icons-left has-icons-right">
+            <div class="select">
+              <select v-model="searchLanguage">
+                <option :value="undefined">ALL</option>
+                <option
+                  v-for="(name, code) in LANGUAGES"
+                  :value="code">{{ name }}</option>
+              </select>
+            </div>
+          </p>
+        </div>
+        <div class="field">
+          <button class="button is-primary" @click="search">Search</button>
+        </div>
+      </div>
+    </div>
+    <oj-pagination
+      :pagination="pagination"
+      @pageClick="pageClick"
+    ></oj-pagination>
     <table class="table">
       <thead>
         <tr>
@@ -51,40 +108,88 @@ import SolutionItem from '../components/SolutionItem.vue'
 import Pagination from '../components/Pagination.vue'
 
 export default {
+  data () {
+    return {
+      searchUid: undefined,
+      searchPid: undefined,
+      searchJudge: undefined,
+      searchLanguage: undefined
+    }
+  },
   components: {
     'oj-pagination': Pagination,
     'oj-solutionitem': SolutionItem
   },
-  props: [ 'page', 'limit' ],
+  props: [ 'page', 'limit', 'judge', 'language', 'uid', 'pid' ],
   computed: {
     solutions () {
       return this.$store.getters.solutionsList
     },
     pagination () {
       return this.$store.getters.solutionsPagination
+    },
+    JUDGES () {
+      return this.$store.getters.judges
+    },
+    LANGUAGES () {
+      return this.$store.getters.languages
     }
   },
   created () {
     document.title = 'Status'
     this.$store.dispatch('fetchSolutionsList', {
       page: this.page,
-      limit: this.limit
+      limit: this.limit,
+      uid: this.uid,
+      pid: this.pid,
+      language: this.language,
+      judge: this.judge
     })
+    this.updateSearch()
   },
   methods: {
     pageClick (page) {
+      // 侧重于 url 上的 query
+      const query = {
+        page,
+        limit: this.limit,
+        uid: this.uid,
+        pid: this.pid,
+        language: this.language,
+        judge: this.judge
+      }
       this.$router.push({
         name: 'status',
-        query: {
-          page
-        }
+        query
       })
       // 同一组件，需要重新 dispatch
       // https://router.vuejs.org/en/essentials/dynamic-matching.html
-      this.$store.dispatch('fetchSolutionsList', {
-        page: page,
-        limit: this.limit
+      this.$store.dispatch('fetchSolutionsList', query)
+    },
+    search () {
+      // 侧重于 search 中的 query
+      const query = {
+        page: 1,
+        limit: this.limit,
+        uid: this.searchUid === '' ? undefined : this.searchUid,
+        pid: this.searchPid === '' ? undefined : this.searchPid,
+        language: this.searchLanguage,
+        judge: this.searchJudge
+      }
+      this.$router.push({
+        name: 'status',
+        query
       })
+      // 同一组件，需要重新 dispatch
+      // https://router.vuejs.org/en/essentials/dynamic-matching.html
+      this.$store.dispatch('fetchSolutionsList', query)
+    },
+    // 其实就是设置初始值
+    updateSearch () {
+      this.searchUid = this.uid
+      this.searchPid = this.pid
+      this.searchJudge = this.judge
+      this.language = this.language
     }
   }
 }
