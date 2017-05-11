cb9d517c571815552b6894b8c8ce26b6226d74cb
lazzzis <shenlijin@outlook.com>
2017-05-11 16:29:22 +0800

    add file submit api

--- controllers/home.js
+++ controllers/home.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** 返回服务器时间 */
 async function servertime (ctx, next) {
   ctx.body = {
@@ -5,6 +7,14 @@ async function servertime (ctx, next) {
   }
 }
 
+async function submit (ctx, next) {
+  const filename = path.basename(ctx.request.body.files.file.path)
+  ctx.body = {
+    path: '/uploads/' + filename
+  }
+}
+
 module.exports = {
-  servertime
+  servertime,
+  submit
 }
