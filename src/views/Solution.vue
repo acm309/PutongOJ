2b45975e23d273f5cb09e8deb65ef06db5ab67e0
lazzzis <shenlijin@outlook.com>
2018-01-05 11:06:27 -0500

    fix code highlight

--- src/views/Solution.vue
+++ src/views/Solution.vue
@@ -7,19 +7,20 @@
       <span>Author: {{ solution.uid }}</span>
     </p>
     <hr>
-    <pre class="error"><code>{{ solution.error }}</code></pre>
-    <Button type="ghost" shape="circle" icon="document" v-clipboard:copy="code" v-clipboard:success="onCopy">
+    <pre class="error" v-if="solution.error"><code>{{ solution.error }}</code></pre>
+    <br>
+    <Button type="ghost" shape="circle" icon="document" v-clipboard:copy="solution.code" v-clipboard:success="onCopy">
       Click to copy code
     </Button>
-    <pre><code v-html="solution.code"></code></pre>
+    <pre><code v-html="prettyCode"></code></pre>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import constant from '../util/constant'
+import constant from '@/util/constant'
 import 'highlight.js/styles/github.css'
-// import highlight from 'highlight.js'
+import highlight from 'highlight.js'
 // https://github.com/isagalaev/highlight.js/issues/1284
 
 export default {
@@ -30,7 +31,10 @@ export default {
   computed: {
     ...mapGetters('solution', [
       'solution'
-    ])
+    ]),
+    prettyCode () {
+      return highlight.highlight('c++', `${this.solution.code}`).value
+    }
   },
   created () {
     this.$store.dispatch('solution/findOne', this.$route.params)
