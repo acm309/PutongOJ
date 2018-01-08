e7a33ba38f2e985cc0c0ba3c4ec37cef08dd67a3
lazzzis <shenlijin@outlook.com>
2018-01-08 14:18:56 -0500

    not resolved: contest edit (this depends on how to implement overview)

--- src/views/Contest/ContestEdit.vue
+++ src/views/Contest/ContestEdit.vue
@@ -52,7 +52,7 @@
         <draggable v-model="contest.list">
           <transition-group name="list">
             <div v-for="(item, index) in contest.list" :key="index" class="list-item">
-              <div>{{ item }} -- {{ jobs.get(item) }}</div>
+              <div>{{ item }} -- {{ jobs.get(+item) }}</div>
               <Icon type="close-circled" @click.native="removeJob(index)"></Icon>
             </div>
           </transition-group>
@@ -61,7 +61,7 @@
     </Row>
     <Row>
       <Col :span="21">
-        <Input v-model="contest.problem" placeholder="Add a pid" @keyup.enter.native="add"></Input>
+        <Input v-model="pid" placeholder="Add a pid" @keyup.enter.native="add"></Input>
       </Col>
       <Col :span="2">
         <Button type="primary" @click="add">Add</Button>
@@ -76,6 +76,8 @@
 
 <script>
 import draggable from 'vuedraggable'
+import isEmpty from 'lodash.isempty'
+import only from 'only'
 import api from '@/api'
 import { mapGetters } from 'vuex'
 
@@ -96,38 +98,40 @@ export default {
           value: 3,
           label: 'Password'
         }
-      ]
+      ],
+      pid: ''
     }
   },
   computed: {
-    ...mapGetters('contest', {
-      contest: 'contest',
-      overview: 'overview'
-    })
+    ...mapGetters('contest', ['contest', 'overview'])
   },
   created () {
-    this.$store.dispatch('contest/find', { cid: this.$route.params.cid })
+    // 这里必须保证此时 overview 是存在的
+    // 如果用户没有点过 overview tab 时，就会出现 overview 不存在的情况
+    let p = Promise.resolve()
+    if (isEmpty(this.overview)) {
+      p = this.$store.dispatch('contest/overview', only(this.$route.params, 'cid'))
+    }
+    p.then(() => this.$store.dispatch('contest/find', only(this.$route.params, 'cid')))
       .then(() => {
         this.overview.forEach((item) => {
-          this.jobs.set(item.pid, item.title)
+          this.jobs.set(+item.pid, item.title)
         })
       })
   },
   methods: {
     add () {
-      this.$store.dispatch('problem/getProblem', { pid: this.contest.problem })
+      this.$store.dispatch('problem/findOne', only(this, 'pid'))
         .then((data) => {
           this.contest.list.push(data.pid)
           this.jobs.set(data.pid, data.title)
+          this.pid = ''
         })
-      this.contest.problem = ''
     },
     submit () {
       api.contest.update(this.contest).then(({ data }) => {
-        if (data.success) {
-          this.$message.success('提交成功！')
-          this.$router.push({name: 'contestOverview', params: { cid: data.cid }})
-        }
+        this.$Message.success('提交成功！')
+        this.$router.push({name: 'contestOverview', params: only(data, 'cid')})
       })
     },
     removeJob (index) {
@@ -136,6 +140,9 @@ export default {
   },
   components: {
     draggable
+  },
+  beforeDestroy () {
+    this.$store.dispatch('contest/findOne', only(this.$route.params, 'cid'))
   }
 }
 </script>
@@ -166,3 +173,11 @@ export default {
   &:hover
     font-size: 20px
 </style>
+
+<docs>
+  这里，所有的更改都到了 store 里的 contest 上，这个方法并不一定最佳实践。
+  一个可能的影响是:
+    如果用户，在表单上修改了比赛的开始时间，比如从 1 月 1 日跳到 2 月 1 日，但是他没点 Submit，也就是说这个数据没有写到数据库，但是如果此时 前端的 contest 已经变了，你可以看到上方的进度条已经变了。如果，此时，用户（没有点 Submit）就点了去看排行榜之类的，那么他看到的都是基于开始时间是 2 月 1 日的。
+  因此，这个时候用户看到的是有问题的！
+  为了，解决这个问题，我在这里，beforeDestroy 处，再次请求了一次比赛。这样，不管用户最终有没有提交修改后的比赛表单，都能保持前端数据与数据库的保持同步。
+</docs>
