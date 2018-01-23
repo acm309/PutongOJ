f339e5e8f0196e209a59804c3222bf993a8f9b0e
lazzzis <shenlijin@outlook.com>
2018-01-22 23:49:07 -0500

    fix bug: 404 status for image upload

--- config/index.js
+++ config/index.js
@@ -14,6 +14,10 @@ module.exports = {
       '/api': {
         target: 'http://127.0.0.1:8888',
         changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://127.0.0.1:8888',
+        changeOrigin: false
       }
     },
 
