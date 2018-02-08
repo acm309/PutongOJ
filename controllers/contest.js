c599106188c9a5962c1d0a9b082a1dd7486f168b
lazzzis <shenlijin@outlook.com>
2018-02-07 22:39:49 -0500

    format

--- controllers/contest.js
+++ controllers/contest.js
@@ -97,51 +97,51 @@ const findOne = async (ctx) => {
 
 // 返回比赛排行榜
 const ranklist = async (ctx) => {
-  const ranklist = {}
+  const contest = ctx.state.contest
+  const ranklist = ctx.state.contest.ranklist
   let res
   const deadline = 60 * 60 * 1000
   const cid = parseInt(ctx.query.cid)
-  const solutions = await Solution.find({
-    mid: cid
-  })
-  for (const solution of solutions) {
-    const { uid } = solution
-    const row = (uid in ranklist) ? ranklist[uid] : { uid }
-    const { pid } = solution
-    const item = (pid in row) ? row[pid] : {}
-    if ('wa' in item) {
-      if (item.wa >= 0) continue
-      if (solution.judge === config.judge.Accepted) {
-        item.wa = -item.wa
-        item.create = solution.create
-      } else item.wa --
-    } else {
-      if (solution.judge === config.judge.Accepted) {
-        item.wa = 0
-        item.create = solution.create
-      } else item.wa = -1
-    }
-    row[pid] = item
-    ranklist[uid] = row
-  }
-  ctx.state.contest.ranklist = ranklist
-  await ctx.state.contest.save()
+  // const solutions = await Solution.find({ mid: cid }).exec()
+  // 临时注释，但请暂时不要删除
+  // for (const solution of solutions) {
+  //   const { uid } = solution
+  //   const row = (uid in ranklist) ? ranklist[uid] : { uid }
+  //   const { pid } = solution
+  //   const item = (pid in row) ? row[pid] : {}
+  //   if ('wa' in item) {
+  //     if (item.wa >= 0) continue
+  //     if (solution.judge === config.judge.Accepted) {
+  //       item.wa = -item.wa
+  //       item.create = solution.create
+  //     } else item.wa --
+  //   } else {
+  //     if (solution.judge === config.judge.Accepted) {
+  //       item.wa = 0
+  //       item.create = solution.create
+  //     } else item.wa = -1
+  //   }
+  //   row[pid] = item
+  //   ranklist[uid] = row
+  // }
+  // ctx.state.contest.ranklist = ranklist
+  // await ctx.state.contest.save()
   await Promise.all(Object.keys(ranklist).map((uid) =>
     User
     .findOne({ uid })
     .exec()
     .then(user => { ranklist[user.uid].nick = user.nick })))
 
-  if (Date.now() + deadline < ctx.state.contest.end) {
-    // 若比赛未进入最后一小时，最新的ranklist推到redis里
+  if (Date.now() + deadline < contest.end) {
+    // 若比赛未进入最后一小时，最新的 ranklist 推到 redis 里
     const str = JSON.stringify(ranklist)
     await redis.lset(`oj:ranklist:${ctx.state.contest.cid}`, 0, str)
     res = ranklist
   } else if (!isAdmin(ctx.session.profile) &&
-    Date.now() + deadline > ctx.state.contest.end &&
-    Date.now() < ctx.state.contest.end) {
+    Date.now() + deadline > contest.end &&
+    Date.now() < contest.end) {
     // 比赛最后一小时封榜，普通用户只能看到题目提交的变化
-    const mid = await redis.lindex(`oj:ranklist:${ctx.state.contest.cid}`, 0)
+    const mid = await redis.lindex(`oj:ranklist:${contest.cid}`, 0)
     res = JSON.parse(mid)
     Object.entries(ranklist).map(([uid, problems]) => {
       Object.entries(problems).map(([pid, sub]) => {
