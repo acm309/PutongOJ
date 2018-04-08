bec8debf9834008e68823328c72554f9dcc26cac
lazzzis <shenlijin@outlook.com>
2018-04-07 21:36:09 -0400

    test: add test suite for status and update news test

--- test/controllers/status.test.js
+++ test/controllers/status.test.js
@@ -5,26 +5,26 @@ const app = require('../../app')
 const server = app.listen()
 const request = supertest.agent(server)
 
-test('News list', async t => {
+test('Status list', async t => {
   const res = await request
-    .get('/api/news/list')
+    .get('/api/status/list')
 
   t.plan(4)
 
   t.is(res.status, 200)
   t.is(res.type, 'application/json')
-  t.truthy(Array.isArray(res.body.list.docs))
+  t.true(Array.isArray(res.body.list.docs))
 
-  if (res.body.list.docs.length > 0) {
-    t.truthy(res.body.list.docs[0].title)
+  if (res.body.list.length > 0) {
+    t.truthy(res.body.list.docs[0].sid)
   } else {
-    t.pass('News list is empty')
+    t.pass('Status list is empty')
   }
 })
 
-test('News fails to find one', async (t) => {
+test('Status fails to find one', async (t) => {
   const res = await request
-    .get('/api/news/-1')
+    .get('/api/status/-1')
 
   t.is(res.status, 400)
   t.is(res.type, 'application/json')
