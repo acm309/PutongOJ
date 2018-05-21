9c6db2efb719599ff6928a5c2e7a2b382713d711
lazzzis <lazzzis@outlook.com>
2018-05-21 21:51:22 +0000

    fix: require unlogined user to login when updating user info

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router')
 const user = require('../controllers/user')
+const { auth } = require('../utils/middlewares')
 
 const router = new Router({
   prefix: '/user'
@@ -10,6 +11,6 @@ router.get('/:uid', user.preload, user.findOne)
 
 // register
 router.post('/', user.create)
-router.put('/:uid', user.preload, user.update)
+router.put('/:uid', auth.login, user.preload, user.update)
 
 module.exports = router
