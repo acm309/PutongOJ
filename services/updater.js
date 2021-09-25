6659fca41402df23509252cbe1adc5d477262876
lazzzis <lazzzis@outlook.com>
2021-09-24 17:37:27 -0700

    fix: fix crash on updater

--- services/updater.js
+++ services/updater.js
@@ -6,41 +6,45 @@ const config = require('../config')
 const redis = require('../config/redis')
 
 async function main () {
+  logger.info('Started!')
   while (1) {
-    const res = await redis.brpop('oj:contest:solution', 365 * 24 * 60) // one year
-    const sid = parseInt(res[1])
-    const solution = await Solution.findOne({ sid }).lean().exec()
-    const { uid, pid } = solution
-    const contest = await Contest.findOne({ cid: solution.mid }).lean().exec()
-    logger.info(`Contest: <${contest.cid}>, Solution: <${sid}>, User: <${uid}>, Problem: <${pid}>`)
-    const { ranklist } = contest
-    if (ranklist[uid] == null) { // 这个用户在这场比赛中第一次提交
-      ranklist[uid] = { uid }
-    }
-    if (ranklist[uid][pid] == null) { // 这个用户在这场比赛第一次提交这道题
-      ranklist[uid][pid] = {}
-    }
-    const grid = ranklist[uid][pid]
-    if (solution.judge === config.judge.Accepted) {
-      if (grid.wa == null) { // 第一次提交就作对了
-        grid.wa = 0
-        grid.create = Date.now()
-      } else if (grid.wa < 0) { // 为了避免出现：已经作对了，有提交该题的情况
-        grid.wa = -grid.wa
-        grid.create = Date.now()
+    try {
+      const res = await redis.brpop('oj:contest:solution', 365 * 24 * 60) // one year
+      const sid = parseInt(res[1])
+      const solution = await Solution.findOne({ sid }).lean().exec()
+      if (solution == null) continue
+      const { uid, pid } = solution
+      const contest = await Contest.findOne({ cid: solution.mid }).lean().exec()
+      logger.info(`Contest: <${contest.cid}>, Solution: <${sid}>, User: <${uid}>, Problem: <${pid}>`)
+      const { ranklist } = contest
+      if (ranklist[uid] == null) { // 这个用户在这场比赛中第一次提交
+        ranklist[uid] = { uid }
       }
-    } else { // 做错
-      if (grid.wa == null) { // 第一次提交就做错了
-        grid.wa = -1
-      } else if (grid.wa < 0) { // 同样，如果 > 0 则代表用户已经作对了，所以对 > 0 的不处理
-        grid.wa -= 1
+      if (ranklist[uid][pid] == null) { // 这个用户在这场比赛第一次提交这道题
+        ranklist[uid][pid] = {}
       }
+      const grid = ranklist[uid][pid]
+      if (solution.judge === config.judge.Accepted) {
+        if (grid.wa == null) { // 第一次提交就作对了
+          grid.wa = 0
+          grid.create = Date.now()
+        } else if (grid.wa < 0) { // 为了避免出现：已经作对了，有提交该题的情况
+          grid.wa = -grid.wa
+          grid.create = Date.now()
+        }
+      } else { // 做错
+        if (grid.wa == null) { // 第一次提交就做错了
+          grid.wa = -1
+        } else if (grid.wa < 0) { // 同样，如果 > 0 则代表用户已经作对了，所以对 > 0 的不处理
+          grid.wa -= 1
+        }
+      }
+      ranklist[uid][pid] = grid
+      await Contest.findOneAndUpdate({ cid: contest.cid }, { ranklist })
+    } catch (e) {
+      logger.error(`${e.message}\n${e.stack}`)
     }
-    ranklist[uid][pid] = grid
-    await Contest.findOneAndUpdate({ cid: contest.cid }, { ranklist })
   }
 }
 
 main()
-  .then(() => logger.info('Started!'))
-  .catch((e) => logger.error(`${e.message}\n${e.stack}`))
