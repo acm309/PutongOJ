42029bb6de2f1869ca9d58f9305855f249b1aedf
luoingly <signature@luoingly.top>
2024-12-06 19:39:53 +0800

    feat: 比赛提交页面的题目选择器
    net-escape/putong-oj#4

--- src/views/Contest/ContestSubmit.vue
+++ src/views/Contest/ContestSubmit.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { storeToRefs } from 'pinia'
-import { inject } from 'vue'
+import { inject, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import OJSubmit from '@/components/Submit'
@@ -14,30 +14,44 @@ const route = useRoute()
 const router = useRouter()
 const $Message = inject('$Message')
 
-const { problems, overview } = $(storeToRefs(contestStore))
-const { solution } = $(storeToRefs(solutionStore))
+const { problems, overview, totalProblems } = storeToRefs(contestStore)
+const { solution } = storeToRefs(solutionStore)
 const { clearCode, create } = solutionStore
 
-const title = $computed(() => overview[+route.params.id - 1]?.title)
+const currentProblemId = computed(() => Number(route.params.id))
+const currentContestId = computed(() => route.params.cid)
+const currentTitle = computed(() => overview.value[currentProblemId.value - 1]?.title)
 
+const pageChange = val => router.push({ name: 'contestSubmit', params: { id: val } })
 const reset = clearCode
 
-async function submit () {
-  await create(Object.assign({}, solution, {
-    pid: problems[+route.params.id - 1],
-    mid: route.params.cid,
-  }))
+async function submit() {
+  await create({
+    ...solution.value,
+    pid: problems.value[currentProblemId.value - 1],
+    mid: currentContestId.value,
+  })
   router.push({
     name: 'contestStatus',
     params: route.params,
   })
-  $Message.info(`submit id:${route.params.id} success!`)
+  $Message.info(`submit id:${currentProblemId.value} success!`)
 }
 </script>
 
 <template>
   <div>
-    <h1>{{ $route.params.id }}:  {{ title }}</h1>
+    <ul>
+      <li
+        v-for="i in totalProblems"
+        :key="i"
+        :class="{ active: i === currentProblemId }"
+        @click="pageChange(i)"
+      >
+        {{ i }}
+      </li>
+    </ul>
+    <h1>{{ currentProblemId }}: {{ currentTitle }}</h1>
     <OJSubmit />
     <Button type="primary" @click="submit">
       {{ t('oj.submit') }}
@@ -53,5 +67,24 @@ h1
   color: #9799ca
   margin-top: 10px
   margin-bottom: 20px
-  text-align:center
+  text-align: center
+ul
+  margin-left: 10px
+  li
+    display: inline-block
+    vertical-align: middle
+    min-width: 32px
+    height: 32px
+    line-height: 30px
+    margin-right: 8px
+    text-align: center
+    list-style: none
+    background-color: #fff
+    border: 1px solid #dddee1
+    border-radius: 4px
+    cursor: pointer
+  .active
+    color: #fff
+    background-color: #e040fb
+    border-color: #e040fb
 </style>
