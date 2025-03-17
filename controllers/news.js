8362a0c8e491a632570e19347a5d9713d5f590ce
luoingly <signature@luoingly.top>
2025-03-17 13:00:55 +0800

    feat: support news visibility control

--- controllers/news.js
+++ controllers/news.js
@@ -1,40 +1,55 @@
 const only = require('only')
+const config = require('../config')
 const News = require('../models/News')
+const { isAdmin } = require('../utils/helper')
 const logger = require('../utils/logger')
 
+/**
+ * 预加载通知信息
+ */
 const preload = async (ctx, next) => {
   const nid = Number.parseInt(ctx.params.nid)
-  if (Number.isNaN(nid)) { ctx.throw(400, 'Nid has to be a number') }
+  if (Number.isNaN(nid)) {
+    ctx.throw(400, 'Nid has to be a number')
+  }
   const news = await News.findOne({ nid }).exec()
-  if (news == null) { ctx.throw(400, 'No such a news') }
+  if (!news) {
+    ctx.throw(400, 'No such a news')
+  }
   ctx.state.news = news
   return next()
 }
 
-// 返回消息列表
+/**
+ * 查询通知列表
+ */
 const find = async (ctx) => {
-  const filter = {}
   const opt = ctx.request.query
   const page = Number.parseInt(opt.page) || 1
   const pageSize = Number.parseInt(opt.pageSize) || 5
+
+  const filter = {}
+  if (!isAdmin(ctx.session.profile)) {
+    filter.status = config.status.Available
+  }
+
   const list = await News.paginate(filter, {
     sort: { nid: -1 },
     page,
     limit: pageSize,
-    select: '-_id',
+    lean: true,
+    leanWithId: false,
+    select: '-_id nid title status create',
   })
-
-  ctx.body = {
-    list,
-  }
+  ctx.body = { list }
 }
 
-// 返回一条消息
+/**
+ * 查询一条通知
+ */
 const findOne = async (ctx) => {
   const news = ctx.state.news
-  ctx.body = {
-    news,
-  }
+  ctx.body = { news: only(news, 'nid title content status create') }
 }
 
 // 新建一条消息
@@ -63,7 +78,7 @@ const create = async (ctx) => {
 const update = async (ctx) => {
   const opt = ctx.request.body
   const news = ctx.state.news
-  const fields = [ 'title', 'content' ]
+  const fields = [ 'title', 'content', 'status' ]
   fields.forEach((field) => {
     news[field] = opt[field]
   })
