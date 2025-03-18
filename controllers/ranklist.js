7d3a232e1e453ed19c76a35e51add92b1fcf4c02
luoingly <signature@luoingly.top>
2025-03-18 13:48:57 +0800

    feat: hide users without any solved in the ranklist

--- controllers/ranklist.js
+++ controllers/ranklist.js
@@ -1,18 +1,23 @@
-const only = require('only')
 const User = require('../models/User')
-const { purify } = require('../utils/helper')
 
-// 返回排名列表
+/**
+ * 返回排行榜
+ */
 const find = async (ctx) => {
   const opt = ctx.request.query
   const page = Number.parseInt(opt.page) || 1
   const pageSize = Number.parseInt(opt.pageSize) || 30
-  const filter = purify(only(opt, 'gid'))
+  const filter = {
+    solve: { $gt: 0 },
+  }
+  if (opt.gid && !Number.isNaN(Number.parseInt(opt.gid))) {
+    filter.gid = Number.parseInt(opt.gid)
+  }
   const list = await User.paginate(filter, {
-    sort: { solve: -1, submit: 1 },
+    sort: { solve: -1, submit: 1, create: 1 },
     page,
     limit: pageSize,
-    select: '-_id -pwd -privilege -timerecord -iprecord -create -__v',
+    select: '-_id uid nick motto submit solve',
     lean: true,
   })
 
