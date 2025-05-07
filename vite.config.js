08afd7b9bbe409146a25a59dc9964cf113fa9626
luoingly <signature@luoingly.top>
2025-05-07 21:09:21 +0800

    feat: update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,87 +1,47 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
-import { visualizer } from 'rollup-plugin-visualizer'
-import vue from '@vitejs/plugin-vue'
-import createImportPlugin from 'vite-plugin-import'
-import { compression } from 'vite-plugin-compression2'
-import legacy from '@vitejs/plugin-legacy'
-
 // https://vitejs.dev/config/
+import path from 'node:path'
+
+import legacy from '@vitejs/plugin-legacy'
+import vue from '@vitejs/plugin-vue'
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   resolve: {
     alias: [
-      {
-        find: /^~/,
-        replacement: '',
-      },
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
+      { find: /^~/, replacement: '' },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
     ],
-    extensions: [
-      '.mjs',
-      '.js',
-      '.ts',
-      '.jsx',
-      '.tsx',
-      '.json',
-      '.vue',
-    ],
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
+    extensions: [ '.mjs', '.js', '.ts', '.json', '.vue' ],
   },
   plugins: [
-    vue({
-      reactivityTransform: true,
-    }),
-    viteCommonjs(),
-    // support browsers which don't support esm
     legacy(),
-    visualizer({
-      // generate stats.html in project root
-      // sourcemap: true,
-      // brotliSize: true,
-      gzipSize: true,
-    }),
-    compression(),
-    createImportPlugin([
-      {
-        libraryName: 'view-ui-plus',
-        libraryDirectory: 'src/components',
-      },
-    ]),
+    ReactivityTransform(),
+    visualizer({ gzipSize: true }),
+    vue(),
   ],
+  css: {
+    preprocessorOptions: {
+      less: { javascriptEnabled: true },
+    },
+  },
   build: {
-    target: 'es2017',
-    reportCompressedSize: false,
-    // sourcemap: true,
-    // https://next.router.vuejs.org/guide/advanced/lazy-loading.html#with-vite
+    chunkSizeWarningLimit: 1024,
+    gzipSize: false,
     rollupOptions: {
       output: {
         manualChunks: {
           common: [ 'vue', 'pinia', 'vue-router', 'axios' ],
-          // 'ui': ['view-ui-plus']
+          interface: [ 'view-ui-plus' ],
         },
       },
     },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8008',
-        changeOrigin: true,
-      },
-      '/uploads': {
-        target: 'http://localhost:8008',
-        changeOrigin: true,
-      },
+      '/api': { target: 'http://localhost:8008', changeOrigin: true },
+      '/uploads': { target: 'http://localhost:8008', changeOrigin: true },
     },
   },
 })
