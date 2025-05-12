0aa907d0717c2d6d44ac086000a886a2ce85bc8c
luoingly <signature@luoingly.top>
2025-05-12 11:45:09 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,27 @@
 const antfu = require('@antfu/eslint-config').default
 const stylistic = require('@stylistic/eslint-plugin')
 
-module.exports = antfu({
+module.exports = antfu(
+  {
     ignores: [
-        '/build/',
-        '/dist/',
-        '/*.js',
-        '/test/unit/coverage/'
-    ]
-}, {
+      '/dist/',
+      '/public/static/',
+    ],
+  },
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
     rules: {
-        'style/max-statements-per-line': ['error', { max: 3 }],
-    }
-}, {
-    rules: {
-        'unused-imports/no-unused-vars': ['warn']
-    }
-}, {
-    rules: {
-        'style/brace-style': ["error", "1tbs", { "allowSingleLine": true }]
-    }
-}, {
-    rules: {
-        'style/space-before-function-paren': ['error', 'always']
-    }
-}, {
-    rules: {
-        'style/array-bracket-spacing': ['error', 'always'],
-    }
-}, {
-    rules: {
-        'antfu/if-newline': ['off']
-    }
-})
+      // Stylistic rules
+      'style/max-statements-per-line': [ 'error', { max: 3 } ],
+      'style/brace-style': [ 'error', '1tbs', { allowSingleLine: true } ],
+      'style/space-before-function-paren': [ 'error', 'always' ],
+      'style/array-bracket-spacing': [ 'error', 'always' ],
+
+      // Other rules
+      'unused-imports/no-unused-vars': [ 'warn' ],
+      'antfu/if-newline': [ 'off' ],
+    },
+  },
+)
