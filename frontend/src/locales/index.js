8a0f7721bde199673df224d036c4b360b4508c4f
luoingly <signature@luoingly.top>
2025-10-05 15:13:44 +0800

    chore: mark previous locale files as legacy

--- frontend/src/locales/index.js
+++ frontend/src/locales/index.js
@@ -1,9 +1,9 @@
 import uiEN from 'view-ui-plus/dist/locale/en-US'
 import uiZH from 'view-ui-plus/dist/locale/zh-CN'
-import ojEN from './en.json'
-import ojZH from './zh.json'
+import ojLegacyEN from './en.legacy.json'
+import ojLegacyZH from './zh.legacy.json'
 
 export default {
-  'zh-CN': { ...uiZH, oj: ojZH },
-  'en-US': { ...uiEN, oj: ojEN },
+  'zh-CN': { ...uiZH, oj: ojLegacyZH },
+  'en-US': { ...uiEN, oj: ojLegacyEN },
 }
