60dd3c0794fcfb7a8f7e68c85810b3c2aaeab66f
luoingly <signature@luoingly.top>
2025-10-06 19:19:15 +0800

    refactor: update layout component

--- frontend/src/components/Layout.vue
+++ frontend/src/components/Layout.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia'
+import Dialog from 'primevue/dialog'
 import SelectButton from 'primevue/selectbutton'
-import { Poptip, Space } from 'view-ui-plus'
-import { computed } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import AuthnDialog from '@/components/AuthnDialog.vue'
 import Header from '@/components/Header.vue'
@@ -12,20 +12,25 @@ import { useHumanLanguage } from '@/utils/helper'
 
 const { locale } = useI18n()
 const rootStore = useRootStore()
+const selectedLang = useHumanLanguage()
+const {
+  website: backend,
+  currentTime,
+  timeDiff,
+} = storeToRefs(rootStore)
 
-const { website: backend, currentTime, timeDiff } = storeToRefs(rootStore)
+const versionDialogVisible = ref(false)
+const serverTime = computed(() => {
+  if (Number.isNaN(timeDiff.value)) {
+    return 'Syncing...'
+  }
+  return `${timePretty(currentTime.value)} (${timeDiffPretty(timeDiff.value)})`
+})
 
-const serverTime = computed(() =>
-  Number.isNaN(timeDiff.value)
-    ? 'Syncing...'
-    : `${timePretty(currentTime.value)} (${timeDiffPretty(timeDiff.value)})`)
 const frontend = {
   buildSHA: import.meta.env.VITE_BUILD_SHA || 'unknown',
   buildTime: Number.parseInt(import.meta.env.VITE_BUILD_TIME) || Date.now(),
-}
-
-let selectedLang = useHumanLanguage()
-locale.value = selectedLang.value
+} as const
 
 const langs = [
   { label: '简体中文', value: 'zh-CN' },
@@ -33,8 +38,12 @@ const langs = [
 ]
 
 function onLangSelected (event: any) {
-  locale.value = selectedLang = event.value
+  locale.value = selectedLang.value = event.value
 }
+
+onMounted(() => {
+  locale.value = selectedLang.value
+})
 </script>
 
 <template>
@@ -48,37 +57,37 @@ function onLangSelected (event: any) {
     <div class="p-8 text-base/relaxed text-center">
       <SelectButton
         v-model="selectedLang" class="block" :options="langs" option-label="label" option-value="value"
-        @change="onLangSelected"
+        :allow-empty="false" @change="onLangSelected"
       />
       <div class="mb-4">
         Server time: {{ serverTime }}
       </div>
-      <Poptip trigger="hover">
+      <div class="cursor-pointer" @click="versionDialogVisible = true">
         <div class="font-bold mb-px text-lg">
           Putong OJ
         </div>
         <div>Copyright &copy; 2017-2025 CJLU ACM Lab.</div>
-        <template #content>
-          <Space direction="vertical">
-            <div>
-              <div class="flex justify-between w-full">
-                <code><b>Putong OJ Backend</b></code>
-                <code>#{{ backend.buildSHA }}</code>
-              </div>
-              <code>Built at {{ timePretty(backend.buildTime) }}</code>
-            </div>
-            <div>
-              <div class="flex justify-between w-full">
-                <code><b>Putong OJ Frontend</b></code>
-                <code>#{{ frontend.buildSHA }}</code>
-              </div>
-              <code>Built at {{ timePretty(frontend.buildTime) }}</code>
-            </div>
-          </Space>
-        </template>
-      </Poptip>
+      </div>
     </div>
   </div>
+  <Dialog v-model:visible="versionDialogVisible" modal header="Info" class="mx-6">
+    <div class="font-mono space-y-4">
+      <div>
+        <div class="flex justify-between">
+          <span class="font-bold">Backend</span>
+          <span>#{{ backend.buildSHA }}</span>
+        </div>
+        <div>Built at {{ timePretty(backend.buildTime) }}</div>
+      </div>
+      <div>
+        <div class="flex justify-between">
+          <span class="font-bold">Frontend</span>
+          <span>#{{ frontend.buildSHA }}</span>
+        </div>
+        <div>Built at {{ timePretty(frontend.buildTime) }}</div>
+      </div>
+    </div>
+  </Dialog>
   <AuthnDialog />
 </template>
 
