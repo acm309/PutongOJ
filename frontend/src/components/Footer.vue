9e75690ae8376e2b0698eb33574b3592abbb0795
luoingly <signature@luoingly.top>
2025-12-07 21:14:32 +0800

    chore: split Footer component from Layout

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import Dialog from 'primevue/dialog'
+import SelectButton from 'primevue/selectbutton'
+import { computed, onMounted, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRootStore } from '@/store'
+import { timeDiffPretty, timePretty } from '@/utils/formate'
+import { useHumanLanguage } from '@/utils/helper'
+
+const { locale } = useI18n()
+const rootStore = useRootStore()
+const selectedLang = useHumanLanguage()
+const {
+  website: backend,
+  currentTime,
+  timeDiff,
+} = storeToRefs(rootStore)
+
+const versionDialogVisible = ref(false)
+const serverTime = computed(() => {
+  if (Number.isNaN(timeDiff.value)) {
+    return 'Syncing...'
+  }
+  return `${timePretty(currentTime.value)} (${timeDiffPretty(timeDiff.value)})`
+})
+
+const frontend = {
+  buildSHA: import.meta.env.VITE_BUILD_SHA || 'unknown',
+  buildTime: Number.parseInt(import.meta.env.VITE_BUILD_TIME) || Date.now(),
+} as const
+
+const langs = [
+  { label: '简体中文', value: 'zh-CN' },
+  { label: 'English', value: 'en-US' },
+]
+
+function onLangSelected (event: any) {
+  locale.value = selectedLang.value = event.value
+}
+
+onMounted(() => {
+  locale.value = selectedLang.value
+})
+</script>
+
+<template>
+  <div class="p-8 text-base/relaxed text-center">
+    <SelectButton
+      v-model="selectedLang" class="block" :options="langs" option-label="label" option-value="value"
+      :allow-empty="false" @change="onLangSelected"
+    />
+    <div class="mb-4">
+      Server time: {{ serverTime }}
+    </div>
+    <div class="cursor-pointer" @click="versionDialogVisible = true">
+      <div class="font-bold mb-px text-lg">
+        Putong OJ
+      </div>
+      <div>Copyright &copy; 2017-2025 CJLU ACM Lab.</div>
+    </div>
+  </div>
+
+  <Dialog v-model:visible="versionDialogVisible" modal header="Info" class="mx-6">
+    <div class="font-mono space-y-4">
+      <div>
+        <div class="flex justify-between">
+          <span class="font-bold">Backend</span>
+          <span>#{{ backend.buildSHA }}</span>
+        </div>
+        <div>Built at {{ timePretty(backend.buildTime) }}</div>
+      </div>
+      <div>
+        <div class="flex justify-between">
+          <span class="font-bold">Frontend</span>
+          <span>#{{ frontend.buildSHA }}</span>
+        </div>
+        <div>Built at {{ timePretty(frontend.buildTime) }}</div>
+      </div>
+    </div>
+  </Dialog>
+</template>
