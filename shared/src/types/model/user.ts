6eb2c0dd7fa6558abd327cecb6320a355941eb5e
luoingly <signature@luoingly.top>
2025-12-27 16:05:22 +0800

    chore: update user avatar model

--- shared/src/types/model/user.ts
+++ shared/src/types/model/user.ts
@@ -3,16 +3,18 @@ import { UserPrivilege } from '../../consts/index.js'
 import { AVATAR_URL_LENGTH_MAX } from '../../consts/limit.js'
 import { isoDatetimeToDate } from '../codec.js'
 
-const UserAvatarSchema = z
-  .url({ protocol: /^https$/, normalize: true })
-  .max(AVATAR_URL_LENGTH_MAX)
+const UserAvatarSchema = z.union([
+  z.url({ protocol: /^https$/, normalize: true }).max(AVATAR_URL_LENGTH_MAX),
+  z.string().startsWith('/').max(AVATAR_URL_LENGTH_MAX),
+  z.literal(''),
+])
 
 export const UserModelSchema = z.object({
   uid: z.string().min(3).max(20).regex(/^[\w-]+$/),
   pwd: z.hex().length(72),
   privilege: z.enum(UserPrivilege),
   nick: z.string().max(30),
-  avatar: z.union([UserAvatarSchema, z.literal('')]),
+  avatar: UserAvatarSchema,
   motto: z.string().max(300),
   mail: z.union([z.email(), z.literal('')]),
   school: z.string().max(30),
