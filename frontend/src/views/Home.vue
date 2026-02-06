9280e7a46024a3791c9c5f65f301cc52db8b8eab
luoingly <signature@luoingly.top>
2026-02-06 16:53:48 +0800

    refactor: new home page design

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,174 +1,104 @@
-<script setup>
-import { storeToRefs } from 'pinia'
-import { Card, Col, Icon, Page, Row, Spin } from 'view-ui-plus'
+<script setup lang="ts">
+import Paginator from 'primevue/paginator'
+import { computed, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
-import { useNewsStore } from '@/store/modules/news'
+import api from '@/api'
 import { timePretty } from '@/utils/formate'
-import { onRouteQueryUpdate, purify } from '@/utils/helper'
+import { onRouteQueryUpdate } from '@/utils/helper'
 
 const { locale, t } = useI18n()
-const isZH = $computed(() => locale.value === 'zh-CN')
-const newsStore = useNewsStore()
 const route = useRoute()
 const router = useRouter()
-const { find } = newsStore
-const { list, sum } = $(storeToRefs(newsStore))
+const zhCN = computed(() => locale.value === 'zh-CN')
 
-const page = $computed(() => Number.parseInt(route.query.page) || 1)
-const pageSize = $computed(() => Number.parseInt(route.query.pageSize) || 5)
-const query = $computed(() => {
-  return purify({
-    page,
-    pageSize,
-  })
-})
+const page = computed(() => Number.parseInt(route.query.page as string) || 1)
+const pageSize = computed(() => Number.parseInt(route.query.pageSize as string) || 10)
 
-let loading = $ref(false)
+const docs = ref([] as { nid: number, title: string, create: number }[])
+const total = ref(0)
+const loading = ref(false)
 
 async function fetch () {
-  loading = true
-  await find(query)
-  loading = false
+  const query = {
+    page: page.value,
+    pageSize: pageSize.value,
+  }
+
+  loading.value = true
+  const resp = await api.news.find(query)
+  loading.value = false
+
+  docs.value = resp.data.list.docs
+  total.value = resp.data.list.total
 }
 
-function reload (payload = {}) {
-  router.push({
-    name: 'home',
-    query: Object.assign(query, purify(payload)),
+function onPage (event: any) {
+  router.replace({
+    query: {
+      ...route.query,
+      page: (event.first / event.rows + 1),
+    },
   })
 }
 
-const pageChange = val => reload({ page: val })
-
-fetch()
+onMounted(fetch)
 onRouteQueryUpdate(fetch)
 </script>
 
 <template>
-  <div class="home-wrap">
-    <div class="news-header">
-      <span v-if="isZH" class="welcome-text">
-        中国计量大学在线评测系统
-      </span>
-      <span v-else class="welcome-text">
-        Welcome to CJLU Online Judge
-      </span>
-      <br>
-      {{ t('oj.news_list') }}
+  <div class="bg-transparent border-none flex flex-col gap-0 lg:gap-4 max-w-4xl md:gap-2 p-0 shadow-none">
+    <div
+      class="-mb-px bg-(--p-content-background) border border-surface lg:shadow-lg md:mb-0 md:p-12 md:rounded-xl md:shadow-md overflow-hidden p-8 relative"
+    >
+      <div class="flex flex-col gap-2 relative text-color z-10">
+        <p class="md:text-xl opacity-90 text-lg">
+          {{ zhCN ? '欢迎访问' : 'Welcome to' }}
+        </p>
+        <h2 class="font-bold text-3xl">
+          {{ zhCN ? '中国计量大学程序设计教学平台' : 'China Jiliang University Online Judge' }}
+        </h2>
+      </div>
+      <div class="-right-10 -top-10 absolute bg-linear-to-tr from-primary h-40 opacity-10 rounded-full to-primary/0 w-40" />
+      <div class="-bottom-20 -left-15 absolute bg-linear-to-bl from-primary h-60 opacity-10 rounded-full to-primary/0 w-60" />
     </div>
-    <div v-if="list.length > 0">
-      <RouterLink
-        v-for="item in list" :key="item.nid"
-        :to="{ name: 'newsInfo', params: { nid: item.nid } }" class="news-link"
-      >
-        <Card dis-hover class="news-card" :class="{ 'news-hidden': item.status === 0 }">
-          <Row type="flex" :gutter="16">
-            <Col flex="68px" class="news-icon">
-              <Icon type="md-paper" class="icon-paper" />
-            </Col>
-            <Col flex="auto" class="news-content">
-              <span class="news-title">{{ item.title }}</span>
-              <p class="news-date">
-                {{ timePretty(item.create, 'yyyy-MM-dd HH:mm') }}
-              </p>
-            </Col>
-          </Row>
-        </Card>
-      </RouterLink>
+
+    <div class="bg-(--p-content-background) border border-surface md:rounded-xl shadow-lg">
+      <div class="p-6">
+        <div class="flex font-semibold gap-4 items-center">
+          <i class="pi pi-megaphone text-2xl" />
+          <h1 class="text-xl">
+            {{ t('ptoj.announcements') }}
+          </h1>
+        </div>
+      </div>
+
+      <template v-if="loading || docs.length === 0">
+        <div class="border-surface border-t flex gap-4 items-center justify-center px-6 py-24">
+          <i v-if="loading" class="pi pi-spin pi-spinner text-2xl" />
+          <span>{{ loading ? t('ptoj.loading') : t('ptoj.empty_content_desc') }}</span>
+        </div>
+      </template>
+
+      <div v-for="doc in docs" v-else :key="doc.nid" class="border-surface border-t flex flex-col gap-2 px-6 py-5">
+        <div class="flex gap-2 text-muted-color text-nowrap text-sm">
+          <i class="pi pi-clock text-sm" />
+          {{ timePretty(doc.create, 'yyyy-MM-dd HH:mm') }}
+        </div>
+        <RouterLink
+          class="font-medium hover:text-primary overflow-hidden text-color text-ellipsis text-lg text-pretty"
+          :to="{ name: 'newsInfo', params: { nid: doc.nid } }"
+        >
+          {{ doc.title }}
+        </RouterLink>
+      </div>
+
+      <Paginator
+        class="border-surface border-t bottom-0 md:rounded-b-xl overflow-hidden sticky z-10"
+        :first="(page - 1) * pageSize" :rows="pageSize" :total-records="total"
+        template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+        :current-page-report-template="t('ptoj.paginator_report')" @page="onPage"
+      />
     </div>
-    <div v-else class="news-empty">
-      <Icon type="ios-planet-outline" class="empty-icon" />
-      <span class="empty-text">{{ t('oj.empty_content') }}</span>
-    </div>
-    <div class="pagination-table">
-      <Page :model-value="page" :total="sum" :page-size="pageSize" show-elevator @on-change="pageChange" />
-    </div>
-    <div class="pagination-mobile">
-      <Page size="small" :model-value="page" :total="sum" :page-size="pageSize" show-elevator @on-change="pageChange" />
-    </div>
-    <Spin size="large" fix :show="loading" class="wrap-loading" />
   </div>
 </template>
-
-<style lang="stylus" scoped>
-.home-wrap
-  width 100%
-  max-width 1024px
-  margin 0 auto
-  padding-top 20px !important
-
-  .news-header
-    font-size 28px
-    padding-bottom 16px
-    .welcome-text
-      font-size 14px
-
-  .news-link
-    text-decoration none
-    color inherit
-  .news-card
-    margin-bottom 20px
-    transition border-color 0.2s ease
-
-    &:hover
-      border-color var(--oj-primary-color) !important
-      .news-date
-        color var(--oj-primary-color)
-
-  .news-hidden
-    .news-icon, .news-title
-      color hsl(0, 0%, 40%) !important
-    &:hover
-      border-color hsl(0, 0%, 60%) !important
-      .news-date
-        color unset
-
-  .news-icon
-    display flex
-    align-items center
-    justify-content center
-    color var(--oj-primary-color)
-    opacity 0.85
-    .icon-paper
-      font-size 32px
-
-  .news-content
-    .news-title
-      color var(--oj-primary-color)
-      font-size 16px
-    .news-date
-      margin-top 7px
-      transition color 0.2s ease
-
-  .pagination-table
-    display block
-  .pagination-mobile
-    display none
-    text-align center
-
-.news-empty
-  margin-bottom 20px
-  padding 32px
-  border 1px solid #dcdee2
-  border-radius 4px
-  display flex
-  align-items center
-  justify-content center
-  .empty-icon
-    font-size 32px
-  .empty-text
-    margin-left 32px
-
-@media screen and (max-width: 1024px)
-  .home-wrap
-    padding-top 10px !important
-
-@media screen and (max-width: 768px)
-  .pagination-table
-    display none !important
-  .pagination-mobile
-    display block !important
-  .news-icon
-    display none !important
-</style>
