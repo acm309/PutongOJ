bd166733d8cfdc74da1078c6f2adf930a64dead3
luoingly <signature@luoingly.top>
2026-09-16 14:38:24 +0800

    feat: refine contest management table

--- apps/web/src/views/Admin/ContestManagement.vue
+++ apps/web/src/views/Admin/ContestManagement.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { AdminContestListQuery, AdminContestListQueryResult } from '@putong-oj/shared'
 import { AdminContestListQuerySchema } from '@putong-oj/shared'
+import { storeToRefs } from 'pinia'
 import Button from 'primevue/button'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
@@ -16,6 +17,7 @@ import { useI18n } from 'vue-i18n'
 import { useRoute, useRouter } from 'vue-router'
 import { findContests } from '@/api/admin'
 import ContestCreateDialog from '@/components/ContestCreateDialog.vue'
+import { useRootStore } from '@/store'
 import { timePretty } from '@/utils/format'
 import { onRouteQueryUpdate } from '@/utils/helper'
 import { useMessage } from '@/utils/message'
@@ -24,6 +26,8 @@ const { t } = useI18n()
 const route = useRoute()
 const router = useRouter()
 const message = useMessage()
+const rootStore = useRootStore()
+const { currentTime } = storeToRefs(rootStore)
 
 const query = ref({} as AdminContestListQuery)
 const docs = ref([] as AdminContestListQueryResult['docs'])
@@ -31,7 +35,18 @@ const total = ref(0)
 const loading = ref(false)
 const createDialog = ref(false)
 
-const booleanOptions = computed(() => [ {
+const hasFilter = computed(() => {
+  return Boolean(
+    query.value.contestId
+    || query.value.title
+    || query.value.course !== undefined
+    || query.value.isHidden !== undefined
+    || query.value.isPublic !== undefined
+    || query.value.isLocked !== undefined,
+  )
+})
+
+const publicOptions = computed(() => [ {
   label: t('ptoj.public'),
   value: true,
 }, {
@@ -57,12 +72,13 @@ const lockedOptions = computed(() => [ {
 
 async function fetch () {
   const parsed = AdminContestListQuerySchema.safeParse(route.query)
-  if (!parsed.success) {
+  if (parsed.success) {
+    query.value = parsed.data
+  } else {
     router.replace({ query: {} })
     return
   }
 
-  query.value = parsed.data
   loading.value = true
   const resp = await findContests(query.value)
   loading.value = false
@@ -80,36 +96,49 @@ async function fetch () {
 function onSearch () {
   router.replace({
     query: {
-      contestId: query.value.contestId,
+      ...route.query,
+      contestId: query.value.contestId ?? undefined,
       title: query.value.title || undefined,
-      course: query.value.course,
+      course: query.value.course ?? undefined,
       isHidden: query.value.isHidden === undefined ? undefined : String(query.value.isHidden),
       isPublic: query.value.isPublic === undefined ? undefined : String(query.value.isPublic),
       isLocked: query.value.isLocked === undefined ? undefined : String(query.value.isLocked),
+      page: undefined,
     },
   })
 }
 
 function onReset () {
-  router.replace({ query: {} })
+  router.replace({
+    query: {
+      ...route.query,
+      contestId: undefined,
+      title: undefined,
+      course: undefined,
+      isHidden: undefined,
+      isPublic: undefined,
+      isLocked: undefined,
+      page: undefined,
+    },
+  })
 }
 
 function onSort (event: any) {
   router.replace({
     query: {
       ...route.query,
-      sortBy: event.field || query.value.sortBy,
-      sort: event.order || query.value.sort,
+      sortBy: event.sortField || event.field || query.value.sortBy,
+      sort: event.sortOrder || event.order || query.value.sort,
       page: undefined,
     },
   })
 }
 
-function onPage (event: { first: number, rows: number }) {
+function onPage (event: any) {
   router.replace({
     query: {
       ...route.query,
-      page: event.first / event.rows + 1,
+      page: (event.first / event.rows + 1),
     },
   })
 }
@@ -127,7 +156,7 @@ onRouteQueryUpdate(fetch)
 </script>
 
 <template>
-  <div class="max-w-[1440px] p-0">
+  <div class="max-w-384 p-0">
     <div class="border-b border-surface p-6">
       <div class="flex font-semibold gap-4 items-center mb-4">
         <i class="p-[4.5px] pi pi-trophy text-2xl" />
@@ -145,36 +174,48 @@ onRouteQueryUpdate(fetch)
           />
         </IconField>
 
-        <InputNumber
-          v-model="query.contestId" fluid :placeholder="t('ptoj.enter_contest_id')" :min="1"
-          :use-grouping="false" :disabled="loading" @keypress.enter="onSearch"
-        />
+        <IconField>
+          <InputNumber
+            v-model="query.contestId" mode="decimal" fluid :placeholder="t('ptoj.filter_by_contest_id')"
+            :min="1" :use-grouping="false" :disabled="loading" @keypress.enter="onSearch"
+          />
+          <InputIcon class="pi pi-hashtag" />
+        </IconField>
 
-        <InputNumber
-          v-model="query.course" fluid :placeholder="t('ptoj.filter_by_course')" :min="-1"
-          :use-grouping="false" :disabled="loading" @keypress.enter="onSearch"
-        />
+        <IconField>
+          <InputNumber
+            v-model="query.course" mode="decimal" fluid :placeholder="t('ptoj.filter_by_course')" :min="-1"
+            :use-grouping="false" :disabled="loading" @keypress.enter="onSearch"
+          />
+          <InputIcon class="pi pi-book" />
+        </IconField>
 
         <Select
-          v-model="query.isPublic" fluid :options="booleanOptions" option-label="label" option-value="value"
-          show-clear :placeholder="t('ptoj.public')" :disabled="loading" @change="onSearch"
+          v-model="query.isPublic" fluid :options="publicOptions" option-label="label" option-value="value"
+          show-clear :placeholder="t('ptoj.filter_by_public')" :disabled="loading" @change="onSearch"
         />
 
         <Select
           v-model="query.isHidden" fluid :options="hiddenOptions" option-label="label" option-value="value"
-          show-clear :placeholder="t('ptoj.hidden')" :disabled="loading" @change="onSearch"
+          show-clear :placeholder="t('ptoj.filter_by_hidden')" :disabled="loading" @change="onSearch"
         />
 
         <Select
           v-model="query.isLocked" fluid :options="lockedOptions" option-label="label" option-value="value"
-          show-clear :placeholder="t('ptoj.locked')" :disabled="loading" @change="onSearch"
+          show-clear :placeholder="t('ptoj.filter_by_locked')" :disabled="loading" @change="onSearch"
         />
 
-        <div class="flex gap-2 items-center justify-end lg:col-span-3 md:col-span-2">
-          <Button icon="pi pi-plus" :label="t('ptoj.create_contest')" :disabled="loading" @click="createDialog = true" />
+        <div class="flex gap-2 items-center justify-end lg:col-span-3 md:col-span-2 xl:col-span-2">
+          <Button
+            icon="pi pi-plus" :label="t('ptoj.create_contest')" :disabled="loading"
+            @click="createDialog = true"
+          />
           <Button icon="pi pi-refresh" severity="secondary" outlined :disabled="loading" @click="fetch" />
-          <Button icon="pi pi-filter-slash" severity="secondary" outlined :disabled="loading" @click="onReset" />
-          <Button icon="pi pi-search" :label="t('ptoj.search')" :disabled="loading" @click="onSearch" />
+          <Button
+            icon="pi pi-filter-slash" severity="secondary" outlined :disabled="loading || !hasFilter"
+            @click="onReset"
+          />
+          <Button :label="t('ptoj.search')" icon="pi pi-search" :disabled="loading" @click="onSearch" />
         </div>
       </div>
     </div>
@@ -183,28 +224,59 @@ onRouteQueryUpdate(fetch)
       class="-mb-px whitespace-nowrap" :value="docs" sort-mode="single" :sort-field="query.sortBy"
       :sort-order="query.sort" :lazy="true" :loading="loading" data-key="contestId" scrollable @sort="onSort"
     >
-      <Column field="contestId" class="font-medium pl-6 w-24" sortable>
+      <Column field="contestId" class="pl-8 text-center w-18" sortable>
         <template #header>
           <i class="pi pi-hashtag" />
         </template>
       </Column>
 
-      <Column :header="t('ptoj.contest')" field="title" class="min-w-80" />
+      <Column :header="t('ptoj.contest')" class="min-w-96">
+        <template #body="{ data }">
+          <span class="-my-1 flex gap-4 items-center justify-between">
+            <Button :label="data.title" link fluid class="justify-start p-0" @click="openContest(data.contestId)" />
+            <span class="flex gap-1 justify-end">
+              <Tag v-if="data.isHidden" :value="t('ptoj.hidden')" severity="secondary" />
+              <Tag v-if="data.isLocked" :value="t('ptoj.locked')" severity="danger" />
+            </span>
+          </span>
+        </template>
+      </Column>
 
       <Column :header="t('oj.course')" class="min-w-48">
         <template #body="{ data }">
-          <span v-if="data.course">{{ data.course.courseId }} · {{ data.course.name }}</span>
+          <span v-if="data.course">{{ data.course.name }}</span>
           <span v-else class="text-muted-color">—</span>
         </template>
       </Column>
 
-      <Column :header="t('ptoj.status')" class="min-w-60">
+      <Column class="text-center w-24">
+        <template #header>
+          <span class="text-center w-full">
+            <i class="pi pi-calendar-clock" />
+          </span>
+        </template>
         <template #body="{ data }">
-          <div class="flex flex-wrap gap-1">
-            <Tag :value="data.isPublic ? t('ptoj.public') : t('ptoj.private')" :severity="data.isPublic ? 'success' : 'warn'" />
-            <Tag v-if="data.isHidden" :value="t('ptoj.hidden')" severity="secondary" />
-            <Tag v-if="data.isLocked" :value="t('ptoj.locked')" severity="danger" />
-          </div>
+          <span v-if="new Date(currentTime) < new Date(data.startsAt)" class="font-bold text-blue-500">
+            {{ t('ptoj.upcoming') }}
+          </span>
+          <span v-else-if="new Date(currentTime) < new Date(data.endsAt)" class="font-bold text-red-500">
+            {{ t('ptoj.running') }}
+          </span>
+          <span v-else class="font-bold">{{ t('ptoj.ended') }}</span>
+        </template>
+      </Column>
+
+      <Column class="text-center w-24">
+        <template #header>
+          <span class="text-center w-full">
+            <i class="pi pi-key" />
+          </span>
+        </template>
+        <template #body="{ data }">
+          <Tag
+            :value="data.isPublic ? t('ptoj.public') : t('ptoj.private')"
+            :severity="data.isPublic ? 'success' : 'warn'" class="-my-2"
+          />
         </template>
       </Column>
 
@@ -217,12 +289,6 @@ onRouteQueryUpdate(fetch)
       <Column :header="t('ptoj.ends_at')" field="endsAt" class="w-48" sortable>
         <template #body="{ data }">
           {{ timePretty(data.endsAt) }}
-        </template>
-      </Column>
-
-      <Column :header="t('ptoj.updated_at')" field="updatedAt" class="pr-4 w-48" sortable>
-        <template #body="{ data }">
-          {{ timePretty(data.updatedAt) }}
         </template>
       </Column>
 
